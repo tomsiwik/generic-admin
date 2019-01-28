e9e7c205fb6d448db5f430840619631d844bbf79
Tomas Sivicki <tomas.sivicki@gmail.com>
2019-01-28 17:35:22 +0100

    build(path): Use public path rather than basepath due to nginx spa rewrites

--- vue.config.js
+++ vue.config.js
@@ -7,5 +7,8 @@ module.exports = {
     appleMobileWebAppCapable: "yes",
     appleMobileWebAppStatusBarStyle: "black"
   },
-  baseUrl: process.env.NODE_ENV === "production" ? "" : "/"
+  /*baseUrl: process.env.NODE_ENV === "production" ? "" : "/"*/
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/'
+    : '/'
 };
